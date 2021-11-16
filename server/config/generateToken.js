c4072e5a754f685e2e7198a163ca009d8e9a5f46
RyFogle <13fogle@gmail.com>
2021-11-16 14:47:02 -0800

    update refresh token

--- server/config/generateToken.js
+++ server/config/generateToken.js
@@ -8,6 +8,16 @@ export const generateAccessToken = (payload) => {
     return jwt.sign(payload, `${process.env.ACCESS_TOKEN_SECRET}`, {expiresIn: '15m'})
 }
 
-export const generateRefreshToken = (payload) => {
-    return jwt.sign(payload, `${process.env.REFRESH_TOKEN_SECRET}`, {expiresIn: '30d'})
+export const generateRefreshToken = (payload, res) => {
+    const refresh_token = jwt.sign(payload, `${process.env.REFRESH_TOKEN_SECRET}`, {expiresIn: '30d'})
+
+    res.cookie('refreshtoken', refresh_token, {
+        sameSite: 'none',
+        secure: true,
+        httpOnly: true,
+        path: `/api/refresh_token`,
+        maxAge: 30*24*60*60*1000
+    })
+
+    return refresh_token;
 }
