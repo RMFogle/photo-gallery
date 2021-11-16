8a48caad8b54382c235c7b9258a62763a7ec4e03
RyFogle <13fogle@gmail.com>
2021-11-16 14:35:49 -0800

    adding herokuapp route

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,5 +1,3 @@
-// export const API_URL = "https://photo-gal-app.herokuapp.com"
-export const API_URL = 'http://localhost:5000'
+export const API_URL = 'https://photo-gal-app.herokuapp.com'
 
-// export const PHOTO_API_URL = 'https://photo-gal-app.herokuapp.com/api/photos';
-export const PHOTO_API_URL = 'http://localhost:5000/api/photos';
+export const PHOTO_API_URL = 'https://photo-gal-app.herokuapp.com/api/photos';
