936995bf6856b60a7a07414938e631026de22904
RyFogle <13fogle@gmail.com>
2021-11-16 17:44:36 -0800

    fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://sad-mestorf-f1ac6b.netlify.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
