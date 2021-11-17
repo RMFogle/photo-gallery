0ec9e0120fcafd26f8a3551c8659e6762d06eeb0
RyFogle <13fogle@gmail.com>
2021-11-16 18:30:17 -0800

    fix

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-import express from 'express'; 
-import mongoose from 'mongoose'; 
-import cors from 'cors';
-import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv';
-
-import routes from "./server/routes/index.js"
-
 dotenv.config(); 
 
-const app = express();
-const port = process.env.PORT || 5000; 
+import express from 'express'; 
+import mongoose from 'mongoose'; 
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import routes from "./server/routes/index.js"
 
+
+const app = express();
 app.use(express.json({ limit: "30mb", extended: true })); 
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors()); 
+app.use(morgan('dev'))
 app.use(cookieParser())
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://sad-mestorf-f1ac6b.netlify.app");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
 
 app.use('/api', routes.authRouter)
 app.use('/api', routes.photosRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello to photo gallery API')
+    res.json({
+        msg: 'Welcome',
+        'photo-gallery-api': 'https://sad-mestorf-f1ac6b.netlify.app'
+    })
 })
+
+const port = process.env.PORT || 5000; 
 
 //Connect to mongodb cloud atlas 
 const uri = process.env.ATLAS_URI;
