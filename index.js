7cb9f88e43e5802e2c29db37164418514659e4a6
RyFogle <13fogle@gmail.com>
2021-11-16 21:44:33 -0800

    try

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ import routes from "./server/routes/index.js"
 
 
 const app = express();
-app.use(express.json({ limit: "30mb", extended: true })); 
-app.use(express.urlencoded({ limit: "30mb", extended: true }))
-
 const corsOptions = {
     origin: 'https://sad-mestorf-f1ac6b.netlify.app',
     optionsSuccessStatus: 200
 }
-
-app.use(cors())
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true })); 
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(morgan('dev'))
 app.use(cookieParser())
 
