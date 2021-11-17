a86460ba8d9c8302ff52863b37b64deaacde062f
RyFogle <13fogle@gmail.com>
2021-11-16 20:40:30 -0800

    another try at cors fix

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ import routes from "./server/routes/index.js"
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true })); 
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
+
+const corsOptions = {
+    origin: 'https://sad-mestorf-f1ac6b.netlify.app',
+    optionsSuccessStatus: 200
+}
+
 app.use(cors())
 app.use(morgan('dev'))
 app.use(cookieParser())
@@ -19,7 +25,7 @@ app.use(cookieParser())
 app.use('/api', routes.authRouter)
 app.use('/api', routes.photosRouter)
 
-app.get('/', (req, res) => {
+app.get('/', cors(corsOptions), (req, res, next) => {
     res.json({
         msg: 'Welcome',
         'photo-gallery-api': 'https://sad-mestorf-f1ac6b.netlify.app'
